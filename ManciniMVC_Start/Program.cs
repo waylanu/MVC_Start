0a6498c60bb48e61dfdd84e2e26cf2c2420d2d2b
ellemancini <ellemancini@yahoo.com>
2021-06-06 22:16:52 -0400

    Initial check-in of module ManciniMVC_Start

---
+++ ManciniMVC_Start/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ManciniMVC_Start
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
